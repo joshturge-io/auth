b003638ab389d1469d0bf763f919b08df75888d8
joshturge <joshturge@hotmail.com>
2020-04-01 21:27:50 +1000

    Use test repo for tests

--- pkg/auth/service_test.go
+++ pkg/auth/service_test.go
@@ -13,79 +13,23 @@ import (
 )
 
 var (
-	user      map[string]string
-	blacklist []string
-	srv       *auth.Service
-	password  = "secret6"
+	srv      *auth.Service
+	password = "secret6"
 )
 
-type testRepository struct{}
-
-func NewTestRepository() repository.DepositWithdrawer {
-	return &testRepository{}
-}
-
-func (tr *testRepository) GetRefreshToken(userId string) (string, error) {
-	return user["refresh"], nil
-}
-
-func (tr *testRepository) GetSalt(userId string) (string, error) {
-	return user["salt"], nil
-}
-
-func (tr *testRepository) GetHash(userId string) (string, error) {
-	return user["hash"], nil
-}
-
-func (tr *testRepository) SetRefreshToken(userId, token string, exp time.Duration) error {
-	user["refresh"] = token
-	user["expiration"] = strconv.FormatInt(time.Now().Add(exp).Unix(), 10)
-	return nil
-}
-
-func (tr *testRepository) SetSalt(userId, salt string) error {
-	user["salt"] = salt
-	return nil
-}
-
-func (tr *testRepository) SetHash(userId, hash string) error {
-	user["hash"] = hash
-	return nil
-}
-
-func (tr *testRepository) SetBlacklist(token string, exp time.Duration) error {
-	blacklist = append(blacklist, token)
-	return nil
-}
-
-func (tr *testRepository) IsBlacklisted(token string) (bool, error) {
-	sort.Strings(blacklist)
-	index := sort.SearchStrings(blacklist, token)
-
-	return index < len(blacklist) && blacklist[index] == token, nil
-}
-
-func (tr *testRepository) RemoveRefreshToken(userId string) error {
-	user["refresh"] = ""
-	user["expiration"] = ""
-	return nil
-}
-
-func (tr *testRepository) WithContext(ctx context.Context) {}
-
 func resetRepo() {
-	user = map[string]string{
+	repository.TestUser = map[string]string{
 		"salt":       "H4jk53hGsk3fj4Dfsj3",
 		"hash":       "dd373f6f7e9338d82a5ccab1be65475c06e97fed63cd59b892024a0a120aa6f0",
 		"refresh":    "Uq_XJB5p5clZ_lAjFVND0oTYT9uFe8plBfGHFGMZ4RI=",
 		"expiration": strconv.FormatInt(time.Now().Add(24*time.Hour).Unix(), 10),
 	}
 
-	blacklist = []string{}
+	repository.TestBlacklist = []string{}
 }
 
 func init() {
-	repo := NewTestRepository()
+	repo := repository.NewTestRepository()
 	srv = auth.NewService("secret", repo, &auth.Options{
 		RefreshTokenLength:     32,
 		JWTokenExpiration:      15 * time.Minute,
@@ -98,7 +42,7 @@ func init() {
 func TestSessionWithChallenge(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Millisecond)
 	defer cancel()
-	session, err := srv.SessionWithChallenge(ctx, "", password)
+	session, err := srv.SessionWithChallenge(ctx, "user", password)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
@@ -111,7 +55,7 @@ func TestSessionWithChallenge(t *testing.T) {
 func TestDestroySession(t *testing.T) {
 	resetRepo()
 
-	jw := token.NewJW("secret", "", 15*time.Minute)
+	jw := token.NewJW("secret", "user", 15*time.Minute)
 	if err := jw.Generate(); err != nil {
 		t.Error(err)
 	}
@@ -119,25 +63,27 @@ func TestDestroySession(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Millisecond)
 	defer cancel()
 
-	if err := srv.DestroySession(ctx, &auth.Session{"", user["refresh"],
+	if err := srv.DestroySession(ctx, &auth.Session{"user", repository.TestUser["refresh"],
 		jw.Token()}); err != nil {
 		t.Error(err)
+		t.FailNow()
 	}
 
-	if i := sort.SearchStrings(blacklist, jw.Token()); blacklist[i] != jw.Token() {
-		t.Error("JWT not in blacklist")
+	if i := sort.SearchStrings(repository.TestBlacklist,
+		jw.Token()); repository.TestBlacklist[i] != jw.Token() {
+		t.Error("JWT not in repository.TestBlacklist")
 	}
 }
 
 func TestRenew(t *testing.T) {
 	resetRepo()
-	jw := token.NewJW("secret", "", 15*time.Minute)
+	jw := token.NewJW("secret", "user", 15*time.Minute)
 	if err := jw.Generate(); err != nil {
 		t.Error(err)
 	}
 	oldSess := &auth.Session{
-		UserId:  "",
-		Refresh: user["refresh"],
+		UserId:  "user",
+		Refresh: repository.TestUser["refresh"],
 		JWT:     jw.Token(),
 	}
 
@@ -149,11 +95,12 @@ func TestRenew(t *testing.T) {
 		t.Error(err)
 	}
 
-	if i := sort.SearchStrings(blacklist, jw.Token()); blacklist[i] != jw.Token() {
-		t.Error("JWT not in blacklist")
+	if i := sort.SearchStrings(repository.TestBlacklist,
+		jw.Token()); repository.TestBlacklist[i] != jw.Token() {
+		t.Error("JWT not in repository.TestBlacklist")
 	}
 
-	if user["refresh"] != newSess.Refresh {
-		t.Error("new refresh has not been set on user")
+	if repository.TestUser["refresh"] != newSess.Refresh {
+		t.Error("new refresh has not been set on repository.TestUser")
 	}
 }
