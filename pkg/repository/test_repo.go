e0fc2bdf636c118f53bf375469c7896cf47c968a
joshturge <joshturge@hotmail.com>
2020-04-01 21:26:56 +1000

    Made test data public

--- pkg/repository/test_repo.go
+++ pkg/repository/test_repo.go
@@ -7,14 +7,14 @@ import (
 	"time"
 )
 
-var user = map[string]string{
+var TestUser = map[string]string{
 	"salt":       "H4jk53hGsk3fj4Dfsj3",
 	"hash":       "dd373f6f7e9338d82a5ccab1be65475c06e97fed63cd59b892024a0a120aa6f0",
 	"refresh":    "Uq_XJB5p5clZ_lAjFVND0oTYT9uFe8plBfGHFGMZ4RI=",
 	"expiration": strconv.FormatInt(time.Now().Add(24*time.Hour).Unix(), 10),
 }
 
-var blacklist = []string{}
+var TestBlacklist = []string{}
 
 type testRepository struct{}
 
@@ -22,56 +22,56 @@ func NewTestRepository() Repository {
 	return &testRepository{}
 }
 
-func (tr *testRepository) GetRefreshToken(userId string) (string, error) {
-	return user["refresh"], nil
+func (tr *testRepository) GetRefreshToken(TestUserId string) (string, error) {
+	return TestUser["refresh"], nil
 }
 
-func (tr *testRepository) GetSalt(userId string) (string, error) {
-	return user["salt"], nil
+func (tr *testRepository) GetSalt(TestUserId string) (string, error) {
+	return TestUser["salt"], nil
 }
 
-func (tr *testRepository) GetHash(userId string) (string, error) {
-	return user["hash"], nil
+func (tr *testRepository) GetHash(TestUserId string) (string, error) {
+	return TestUser["hash"], nil
 }
 
-func (tr *testRepository) SetRefreshToken(userId, token string, exp time.Duration) error {
-	user["refresh"] = token
-	user["expiration"] = strconv.FormatInt(time.Now().Add(exp).Unix(), 10)
+func (tr *testRepository) SetRefreshToken(TestUserId, token string, exp time.Duration) error {
+	TestUser["refresh"] = token
+	TestUser["expiration"] = strconv.FormatInt(time.Now().Add(exp).Unix(), 10)
 	return nil
 }
 
-func (tr *testRepository) SetSalt(userId, salt string) error {
-	user["salt"] = salt
+func (tr *testRepository) SetSalt(TestUserId, salt string) error {
+	TestUser["salt"] = salt
 	return nil
 }
 
-func (tr *testRepository) SetHash(userId, hash string) error {
-	user["hash"] = hash
+func (tr *testRepository) SetHash(TestUserId, hash string) error {
+	TestUser["hash"] = hash
 	return nil
 }
 
 func (tr *testRepository) SetBlacklist(token string, exp time.Duration) error {
-	blacklist = append(blacklist, token)
+	TestBlacklist = append(TestBlacklist, token)
 	return nil
 }
 
 func (tr *testRepository) IsBlacklisted(token string) (bool, error) {
-	sort.Strings(blacklist)
-	index := sort.SearchStrings(blacklist, token)
+	sort.Strings(TestBlacklist)
+	index := sort.SearchStrings(TestBlacklist, token)
 
-	return index < len(blacklist) && blacklist[index] == token, nil
+	return index < len(TestBlacklist) && TestBlacklist[index] == token, nil
 }
 
-func (tr *testRepository) RemoveRefreshToken(userId string) error {
-	user["refresh"] = ""
-	user["expiration"] = ""
+func (tr *testRepository) RemoveRefreshToken(TestUserId string) error {
+	TestUser["refresh"] = ""
+	TestUser["expiration"] = ""
 	return nil
 }
 
 func (tr *testRepository) WithContext(ctx context.Context) {}
 
 func (tr *testRepository) Close() error {
-	user = nil
-	blacklist = []string{}
+	TestUser = nil
+	TestBlacklist = []string{}
 	return nil
 }
