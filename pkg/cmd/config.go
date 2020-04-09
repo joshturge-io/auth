8fafea2f9bb03775c670e2085930960d2e83958d
joshturge <joshturge@hotmail.com>
2020-04-09 13:36:34 +1000

    Cleaned up config defaults method

--- pkg/cmd/config.go
+++ pkg/cmd/config.go
@@ -16,26 +16,30 @@ type Configuration struct {
 	Token   TokenConfig
 }
 
+// SetDefaults will set the defaults for our config struct
+func (c *Configuration) SetDefaults() {
+	switch {
+	case c.Repo.FlushInterval == 0:
+		c.Repo.FlushInterval = 15
+	case c.Cipher.SaltLength == 0:
+		c.Cipher.SaltLength = 16
+	case c.Token.Refresh.Expiration == 0:
+		c.Token.Refresh.Expiration = 24
+	case c.Token.Refresh.Length == 0:
+		c.Token.Refresh.Length = 32
+	case c.Token.Jwt.Expiration == 0:
+		c.Token.Jwt.Expiration = 15
+	}
+}
+
 type RepositoryConfig struct {
 	Address       string
 	FlushInterval int
 }
 
-func (rc *RepositoryConfig) SetDefaults() {
-	if rc.FlushInterval == 0 {
-		rc.FlushInterval = 15
-	}
-}
-
 type CipherConfig struct {
 	SaltLength int
 	Keys       []string
-}
-
-func (ac *CipherConfig) SetDefaults() {
-	if ac.SaltLength == 0 {
-		ac.SaltLength = 16
-	}
 }
 
 type TokenConfig struct {
@@ -48,47 +52,28 @@ type RefreshConfig struct {
 	Expiration int
 }
 
-// SetDefaults will set the defaults for a refresh token if no configuration exists
-func (rc *RefreshConfig) SetDefaults() {
-	if rc.Length == 0 {
-		rc.Length = 32
-	} else if rc.Expiration == 0 {
-		rc.Expiration = 24
-	}
-}
-
 type JWTConfig struct {
 	Expiration int
-}
-
-// SetDefaults will set the defaults for a jwt if no configuration exists
-func (jc *JWTConfig) SetDefaults() {
-	if jc.Expiration == 0 {
-		jc.Expiration = 15
-	}
 }
 
 // ParseConfig will look for a config file in a specified directory.
 // Returns ErrConfigNotExist when the configuration file can't be found
 func ParseConfig(path string) (*Configuration, error) {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(path)
+	v := viper.New()
+	v.SetConfigName("config")
+	v.AddConfigPath(path)
 
-	config := &Configuration{}
-
-	if err := viper.ReadInConfig(); err != nil {
+	if err := v.ReadInConfig(); err != nil {
 		return nil, ErrConfigNotExist
 	}
 
-	if err := viper.Unmarshal(&config); err != nil {
+	config := &Configuration{}
+
+	if err := v.Unmarshal(config); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal configuration file: %w", err)
 	}
 
-	// set defaults if none exist
-	config.Token.Refresh.SetDefaults()
-	config.Token.Jwt.SetDefaults()
-	config.Cipher.SetDefaults()
-	config.Repo.SetDefaults()
+	config.SetDefaults()
 
 	return config, nil
 }
